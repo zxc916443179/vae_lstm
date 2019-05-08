3ad2c6fb3d9537a7e4a11025299b2ef18244bd1b
zxc916443179 <zxc916443179@163.com>
2019-05-08 16:18:26 +0800

    add device

--- eval.py
+++ eval.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import os, platform
 from model import Utils
 
 tf.flags.DEFINE_string("dataset_path", './UCSDped_patch/ped1', "dataset path")
@@ -6,8 +7,11 @@ tf.flags.DEFINE_integer("max", 10000, "max number of dataset")
 tf.flags.DEFINE_string("checkpoint_dir", "none", "loading latest checkpoint")
 tf.flags.DEFINE_string('label_dir', './label/label15.p', "dir of label")
 tf.flags.DEFINE_integer('batch_size', 128, 'batch size')
+tf.flags.DEFINE_string('device', '0', 'cuda visible devices')
 
 flags = tf.flags.FLAGS
+if 'Linux' in platform.system():
+    os.environ["CUDA_VISIBLE_DEVICES"] = flags.device
 
 graph = tf.Graph()
 graph.as_default()
