98eb386f2391d278f8245ef082ee4cbc3346ab41
zxc916443179 <zxc916443179@163.com>
2019-05-09 11:40:46 +0800

    write lines

--- eval.py
+++ eval.py
@@ -42,7 +42,8 @@ with graph.as_default():
                 input_x: x, training: True
             })                            
             log = 'psnr:%.5f \t kl:%.5f \t recon:%.5f' % (psnr_loss, kl_loss, np.mean(recon_loss))
-            f.writelines(i for i in recon_loss)
+            for i in recon_loss:
+                f.writelines(i)
             print(log)
         psnr_loss, kl_loss, recon_loss, recon_out = sess.run([psnr, kl, recon, out], feed_dict={
             input_x: x, training: True
