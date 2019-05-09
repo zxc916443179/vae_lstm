3d75dcd62476ab0cd60c97190f32639be355db44
zxc916443179 <zxc916443179@163.com>
2019-05-09 11:09:26 +0800

    imsave and recon loss

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ import os, platform
 from model import Utils
 import utils
 import numpy as np
+import scipy
 tf.flags.DEFINE_string("dataset_path", './UCSDped_patch/ped1', "dataset path")
 tf.flags.DEFINE_integer("max", 10000, "max number of dataset")
 tf.flags.DEFINE_string("checkpoint_dir", "none", "loading latest checkpoint")
@@ -32,12 +33,22 @@ with graph.as_default():
         input_x = graph.get_operation_by_name('input_x').outputs[0]
         training = graph.get_operation_by_name('training').outputs[0]
         data = utils.read_data_UCSD(flags.dataset_path, shuffle=True, reshape=False)
-
+        out = graph.get_operation_by_name('score/Reshape_1').outputs[0]
+        input_x_ = graph.get_operation_by_name('score/Reshape').outputs[0]
+        recon = tf.reduce_mean(tf.reduce_sum(tf.square(input_x_ - recon), 1))
         for batch in utils.batch_iter(data, 128, shuffle=True):
             x = np.asarray(batch)
-            psnr_loss, kl_loss = sess.run([psnr, kl], feed_dict={
+            psnr_loss, kl_loss, recon_loss = sess.run([psnr, kl, recon], feed_dict={
                 input_x: x, training: True
-            })
-            log = 'psnr:%.5f \t kl:%.5f' % (psnr_loss, kl_loss)
+            })                            
+            log = 'psnr:%.5f \t kl:%.5f \t recon:%.5f' % (psnr_loss, kl_loss, recon_loss)
             f.writelines(log)
             print(log)
+        psnr_loss, kl_loss, recon_loss, recon_out = sess.run([psnr, kl, recon, out], feed_dict={
+            input_x: x, training: True
+        })
+        log = 'psnr:%.5f \t kl:%.5f \t recon:%.5f' % (psnr_loss, kl_loss, recon_loss)
+        print(log)
+        scipy.misc.imsave('./input.jpg', utils.montage(x))
+        recon_out = np.squeeze(recon_out, -1)
+        scipy.misc.imsave('./generate.jpg', utils.montage(x))
