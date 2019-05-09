a870ef0102e5d6a08bb53ae802b2d6096f402bc5
zxc916443179 <zxc916443179@163.com>
2019-05-09 16:41:31 +0800

    option of whether fix lr

--- convVae_tf.py
+++ convVae_tf.py
@@ -184,7 +184,10 @@ class VAE(object):
         train_data = train_data[:-128]
         global_step = tf.Variable(0, trainable=False, name='global_step')
         # optimizer definition
-        learning_rate = tf.train.exponential_decay(self.learning_rate, global_step, 4000, 0.999, staircase=True)
+        if flags.fixed_lr:
+            learning_rate = self.learning_rate
+        else:
+            learning_rate = tf.train.exponential_decay(self.learning_rate, global_step, 4000, 0.999, staircase=True)
         train_op = tf.train.AdamOptimizer(learning_rate).minimize(self.loss, global_step)
         # optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
         # train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(self.loss, global_step)
