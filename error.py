f1eaf320e961eee7357d257082682df1799bef05
zxc916443179 <zxc916443179@163.com>
2019-05-19 20:27:44 +0800

    debug

--- error.py
+++ error.py
@@ -1,5 +1,5 @@
 
 class ModeNotDefinedError(Exception):
     def __init__(self, mode, message='given mode not found, expect (train or finetune) but get '):
-        self.mode = mode
+        self.mode = message + mode
         self.message = message + self.mode
