78708d580dba8cab76c29e6fc9d5dea4e65752c7
zxc916443179 <zxc916443179@163.com>
2019-05-22 11:23:28 +0800

    auroc

---
+++ auroc.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import roc_curve, auc
+import os
+gt = [
+    [61, 180],
+    [95, 180],
+    [1, 146],
+    [31, 180],
+    [1, 129],
+    [1, 159],
+    [46, 180],
+    [1, 180],
+    [1, 120],
+    [1, 150],
+    [1, 180],
+    [88, 180]
+]
+
+dirs = os.listdir(os.path.join('UCSDped_patch', 'ped2', 'test'))
+print(dirs)
+for d in dirs:
+    for img_dir in os.listdir(os.path.join('UCSDped_patch', 'ped2', 'test', d, 'box_img')):
+        print(img_dir)
